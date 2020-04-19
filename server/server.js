3f3e876c401e45fe68887d5a6f6e064a71fb0723
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-18 18:42:57 -0600

    server set up

--- server/server.js
+++ server/server.js
@@ -3,7 +3,27 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 
+//routes
+
+//app and port
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // ==== middleware
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// ==== serve static files
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+// ==== handle page refresh
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+//routes
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
