9faa2c480170ddd734144eff8248071f844d632b
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-21 16:04:21 -0600

    updated actions/reducers for profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,20 +1,18 @@
-import { FETCH_PROFILE, UPDATE_ERROR } from './types.js';
+import { actions } from './types.js';
 import Axios from 'axios';
 
 export function fetchProfile() {
     return function(dispatch) {
-        console.log('fetching')
         Axios.get('/api/profile/me')
             .then(response => {
-                console.log(response)
                 dispatch({
-                    type: FETCH_PROFILE,
+                    type: actions.profile.FETCH_PROFILE,
                     payload: response.data
                 });
             })
             .catch(error => {
                 dispatch({
-                    type: UPDATE_ERROR,
+                    type: actions.error.UPDATE_ERROR,
                     payload: error
                 });
             });
