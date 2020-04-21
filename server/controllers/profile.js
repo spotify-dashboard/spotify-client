f6924fa8289bee304458b7bfba2c7a5fec7c9e63
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-21 13:01:53 -0600

    updated profile handler

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -4,10 +4,8 @@ const login = require('../controllers/login.js');
 module.exports = {
     profile: {
         get: (req, res) => {
-            // console.log(login.credentials)
             login.credentials.getMe()
                 .then(data => {
-                    console.log(data)
                     res.status(200).json(data.body)
                 })
                 .catch(err => {
