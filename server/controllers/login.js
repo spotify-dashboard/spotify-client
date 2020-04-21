7b8d0e53139527dea54e044130f305bac792974b
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-21 13:01:17 -0600

    exporting login credentials for other routes

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -10,7 +10,26 @@ let spotifyApi = new SpotifyWebApi({
   redirectUri: 'http://localhost:8080/login/callback'
 });
 
-scopes = ['user-read-private', 'user-read-email','playlist-modify-public','playlist-modify-private'];
+scopes = [
+    'user-read-private', //profile info
+    'user-read-email',
+    'playlist-modify-public', //playlists
+    'playlist-modify-private',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-library-read', //user library
+    'user-library-modify',
+    'user-top-read', // listening history
+    'user-read-recently-played',
+    'user-read-playback-position',
+    'user-follow-read', // follow
+    'user-follow-modify',
+    'streaming', // playback
+    'app-remote-control',
+    'user-read-playback-state', // spotify connection
+    'user-modify-playback-state',
+    'user-read-currently-playing'
+];
 
 
 module.exports = {
@@ -24,13 +43,15 @@ module.exports = {
         try {
           var data = await spotifyApi.authorizationCodeGrant(code)
           const { access_token, refresh_token } = data.body;
+
+          console.log('access token++', access_token)
           
           spotifyApi.setAccessToken(access_token);
           spotifyApi.setRefreshToken(refresh_token);
       
           res.redirect('http://localhost:8080/success');
 
-          res.status(200).send(spotifyApi);
+          res.status(200).send({message: "Signed in successfully"});
         } catch(err) {
           res.redirect('/#/error/invalid token');
         }
