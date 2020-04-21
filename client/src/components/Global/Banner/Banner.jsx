9fef6e502afdad31e4395f2e036c905cb8ab7a8f
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-21 16:03:38 -0600

    integrated redux

--- client/src/components/Global/Banner/Banner.jsx
+++ client/src/components/Global/Banner/Banner.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import store from '../../../store.js';
+import { connect } from 'react-redux';
 import styles from './banner.module.scss';
 import Login from '../../Login/Login.jsx';
 
 const Banner = props => {
-
     return (
         <div className={styles.banner}>
             <div className="flex flex-vertical-align flex-spread">
@@ -21,4 +22,8 @@ const Banner = props => {
     )
 };
 
-export default Banner;
+const mapStateToProps = state => {
+    return { profile: state.getProfile.profile };
+}
+
+export default connect(mapStateToProps)(Banner);
