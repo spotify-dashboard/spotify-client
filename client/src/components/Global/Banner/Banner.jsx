fa7bcc70b3ba244ce46bc6410b3cfd9e117eb73e
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-20 20:07:09 -0600

    displaying user data

--- client/src/components/Global/Banner/Banner.jsx
+++ client/src/components/Global/Banner/Banner.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './banner.module.scss';
+import Login from '../../Login/Login.jsx';
 
 const Banner = props => {
 
     return (
         <div className={styles.banner}>
-            <h1>Banner</h1>
+        
+        {props.profile &&
+            <div className="flex flex-vertical-align flex-spread">
+                <div className="flex flex-vertical-align">
+                {props.profile.images !== undefined &&
+                    <img className={styles.profileImg} src={props.profile.images[0].url} />
+                }
+                <h1 className={styles.profileName}>{props.profile.display_name}</h1>
+                </div>
+                <Login />
+            </div>
+        }
+            
         </div>
     )
 };
