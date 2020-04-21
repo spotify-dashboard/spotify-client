90333254ca103b58b6d5ae56edf658d9935495ae
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-20 21:01:13 -0600

    added chart comp and style

---
+++ client/src/components/DataViz/Chart/Chart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './chart.module.scss';
+
+const Chart = props => {
+    return (
+        <div className={styles.chartModule}>
+            <h3>Chart</h3>
+        </div>
+    )
+};
+
+export default Chart;
