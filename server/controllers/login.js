f45d4411c1246c2bde42696ce7db50e6d1a509e9
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-20 20:05:59 -0600

    authentication updates for login

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,5 +1,40 @@
+const Axios = require('axios');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+require('dotenv').config();
+
+// credentials are optional
+let spotifyApi = new SpotifyWebApi({
+  clientId: process.env.MY_CLIENT_ID,
+  clientSecret: process.env.MY_CLIENT_SECRET,
+  redirectUri: 'http://localhost:8080/login/callback'
+});
+
+scopes = ['user-read-private', 'user-read-email','playlist-modify-public','playlist-modify-private'];
+
+
 module.exports = {
     login: (req, res) => {
-        res.status(200).json('success');
+        var html = spotifyApi.createAuthorizeURL(scopes)
+        res.redirect(html+"&show_dialog=true");
+    },
+    callback: async (req,res) => {
+        const { code } = req.query;
+        
+        try {
+          var data = await spotifyApi.authorizationCodeGrant(code)
+          const { access_token, refresh_token } = data.body;
+          
+          spotifyApi.setAccessToken(access_token);
+          spotifyApi.setRefreshToken(refresh_token);
+      
+          res.redirect('http://localhost:8080/success');
+
+          res.status(200).send(spotifyApi);
+        } catch(err) {
+          res.redirect('/#/error/invalid token');
+        }
     }
 }
+
+module.exports.credentials = spotifyApi;
--- server/routes/login.js
+++ server/routes/login.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const loginController = require('../controllers/login.js');
 
 router.get('/', loginController.login);
+router.get('/callback', loginController.callback);
 
 module.exports = router;
