59dc307236b22f4f7920db29004253911a90349b
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-20 20:33:11 -0600

    styling changes

--- client/src/components/HomeDashboard/HomeDashboard/HomeDashboard.jsx
+++ client/src/components/HomeDashboard/HomeDashboard/HomeDashboard.jsx
@@ -3,10 +3,11 @@ import Main from '../Main/Main.jsx';
 import SidebarLeft from '../../Sidebars/SidebarLeft/SidebarLeft.jsx';
 import SidebarRight from '../../Sidebars/SidebarRight/SidebarRight.jsx';
 import Footer from '../../Global/Footer/Footer.jsx';
+import styles from './homedashboard.module.scss';
 
 const HomeDashboard = props => {
     return(
-        <div className="homeDashboard">
+        <div className={styles.homeDashboard}>
             <div className="flex">
                 <SidebarLeft/>
                 <Main profile={props.profile} />
