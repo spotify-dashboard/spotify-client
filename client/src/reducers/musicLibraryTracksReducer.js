c56f766358cc5eb21a803e6cf9f15343aab3e101
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-22 10:47:41 -0600

    added music library reducer

---
+++ client/src/reducers/musicLibraryTracksReducer.js
@@ -0,0 +1,23 @@
+import { actions } from '../actions/types.js';
+
+const initialState = {
+    music_library_tracks: [],
+    error: null
+};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case actions.music_library.tracks.FETCH_LIBRARY:
+            return {
+                ...state,
+                music_library_tracks: action.payload
+            }
+        case actions.error.UPDATE_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+};
