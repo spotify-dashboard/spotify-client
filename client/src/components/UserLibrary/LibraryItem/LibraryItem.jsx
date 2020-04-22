5c020968d0e565d875430b3257df41e1047974f7
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-22 10:48:03 -0600

    added comp to render user music library item

---
+++ client/src/components/UserLibrary/LibraryItem/LibraryItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LibraryItem = props => {
+    
+    console.log('item props', props);
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+export default LibraryItem;
