43fe384bf8336915a79cfcd93642d86662bedaa1
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-23 01:45:01 -0600

    removing console log

--- client/src/actions/musicLibraryActions.js
+++ client/src/actions/musicLibraryActions.js
@@ -5,7 +5,7 @@ export function fetchMusicLibraryTracks() {
     return function(dispatch) {
         Axios.get('/api/user-library/tracks')
                 .then(response => {
-                    console.log(response);
+                    
                     dispatch({
                         type: actions.music_library.tracks.FETCH_LIBRARY,
                         payload: response.data
