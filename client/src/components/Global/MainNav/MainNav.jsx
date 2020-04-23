a1582efc6cca6b144fd0c82ac5cfbbf1a39ef74f
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-22 18:33:57 -0600

    updated main nav to change views on click

---
+++ client/src/components/Global/MainNav/MainNav.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './mainnav.module.scss';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'; // connect to store
+import { pageChange } from '../../../actions/pageChangeActions.js';
+
+const MainNav = props => {
+    console.log('mainNav', props)
+    if (props.navItems !== undefined) {
+        return (
+            <nav className={styles.navbar}>
+                <ul className={styles.navList}>
+                    {props.navItems.map(navItem => {
+                        return (
+                            <li 
+                                key={navItem.id}
+                                onClick={() => props.pageChange(navItem.navLink)} 
+                                className={styles.navItem}
+                            >
+                                    {navItem.navItem}
+                            </li>
+                        )
+                    })}
+                </ul>
+                
+            </nav>
+        )
+    } else {
+        return (
+            <nav>
+                <h1>Please Sign In</h1>
+            </nav>
+        )
+    }
+};
+
+const mapStateToProps = (state, ownProps) => {
+    return { 
+        currentPage: state.pageChange.currentPage,
+        navItems: ownProps.navItems
+    };
+};
+
+const mapDispatchToProps = {
+    pageChange
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainNav);
