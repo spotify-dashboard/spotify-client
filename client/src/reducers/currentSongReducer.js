c01ba2ef334146f5dc3a7cfb202323e5ce73e733
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-23 01:44:40 -0600

    added reducer for getting current song

---
+++ client/src/reducers/currentSongReducer.js
@@ -0,0 +1,23 @@
+import { actions } from '../actions/types.js';
+
+const initialState = {
+    currentSong: [],
+    error: null
+};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case actions.currentSong.CURRENT_SONG:
+            return {
+                ...state,
+                currentSong: action.payload
+            }
+        case actions.error.UPDATE_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+};
