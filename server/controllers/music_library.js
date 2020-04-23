98874c5b608eb22098149f50feaa0fc5cceb8610
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-23 01:43:39 -0600

    removed console

--- server/controllers/music_library.js
+++ server/controllers/music_library.js
@@ -4,16 +4,55 @@ module.exports = {
     music_library: {
         tracks: {
             get: (req, res) => {
-                login.credentials.getMySavedTracks({
+
+                let offset = 1;
+                let dataArr = [];
+
+                const getTracks = login.credentials.getMySavedTracks({
                     limit : 20,
-                    offset: 1
+                    offset: offset
                 })
                 .then(data => {
-                    res.status(200).json(data);
+
+                    const getData = () => {
+                        while (data.body.total > offset) {
+                            
+                            //run api calls until song count ends
+                            login.credentials.getMySavedTracks({
+                                limit : 20,
+                                offset: offset
+                            })
+                            .then(results => {
+                                dataArr.push(results.body.items);
+                                // console.log(dataArr)
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            });
+                            
+                            // increment
+                            offset += 20;
+                        };
+
+                    }
+                    
+                    
+                    getData();
+                    console.log('data arr after func', dataArr)
+                    // res.status(200).json(data);
                 })
                 .catch(err => {
-                    res.status(400).json(err);
+                    console.log(err)
+                    // res.status(400).json(err);
                 });
+
+                // Promise.all(getTracks)
+                //     .then(results => {
+                //         console.log('final data', dataArr)
+                //     })
+                //     .catch(err => {
+                //         console.log(err)
+                //     })
             }
         }
     }
