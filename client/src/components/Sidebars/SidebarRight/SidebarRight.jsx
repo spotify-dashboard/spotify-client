b9d1697afe3ef35ff6652edbbe4bd96dd6380fee
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-23 13:04:06 -0600

    rendering current song comp

--- client/src/components/Sidebars/SidebarRight/SidebarRight.jsx
+++ client/src/components/Sidebars/SidebarRight/SidebarRight.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './sidebarright.module.scss';
 import sharedStyles from '../sidebars.shared.scss';
+import CurrentSong from '../../Global/CurrentSong/CurrentSong.jsx';
 
 const SidebarRight = props => {
+    
     return(
         <div className={styles.sidebarRight}>
             <div className="sidebarModule">
-            <h1 className={styles.header}>Sidebar Right</h1>
+            <CurrentSong />
             <div className={sharedStyles.sidebarContent}>
             
             </div>
