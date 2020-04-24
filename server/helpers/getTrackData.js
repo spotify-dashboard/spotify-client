3c1cdb4ef47bdaee5c0e05e569bf6e6ad0f3ec9c
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-24 15:02:51 -0600

    created generic helper to get all track data from api endpoints

---
+++ server/helpers/getTrackData.js
@@ -0,0 +1,49 @@
+const login = require ('../controllers/login.js'); // credentials
+const Axios = require('axios');
+
+module.exports.getTrackData = (url, params) => {
+    return new Promise( async (resolve, reject) => {
+
+        //set number of iterations for loop; will change after first api call to the max number of tracks
+        let totalTracks = 30;
+        //starting position for the call, will incrememt
+        let offset = 1;
+
+        //holds all track data
+        let dataArr = [];
+        
+        //while more tracks are available
+        while (totalTracks >= offset) {
+            const getTracks = await Axios.get(url, {
+                headers: {
+                    "Authorization": 'Bearer ' + login.credentials._credentials.accessToken
+                },
+                params: {
+                    //O(log n) optimization 
+                    limit: (totalTracks - offset) < 50 ? 1: 50,
+                    offset: offset
+                }
+            })
+            .then(results => {
+                //set total tracks to the total in the res obj, actual #
+                totalTracks = results.data.total;
+                //push to arr
+                dataArr.push(results.data.items);
+            })
+            .catch(error => {
+                console.log('error getting tracks in getTrackData.js', error);
+                reject(error);
+            });
+
+            //O(log n) optimization
+            //increment
+            offset += (totalTracks - offset) < 50 ? 1: 50;
+        }
+
+        //flatten and serve
+        let flattenedArr = dataArr.flat(Infinity)
+        
+        //return
+        resolve(flattenedArr);
+    });
+};
