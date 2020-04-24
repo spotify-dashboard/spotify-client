34af983cb59b844966ab5443c1ce8048e4c259e6
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-24 16:47:37 -0600

    created action dispatch for getting all playlists and by id

---
+++ client/src/actions/playlistActions.js
@@ -0,0 +1,38 @@
+import { actions } from './types.js';
+import Axios from 'axios';
+
+export function getAllPlaylists() {
+    return function(dispatch) {
+        Axios.get('/api/playlists/')
+            .then(response => {
+                dispatch({
+                    type: actions.playlists.get.GET_ALL_PLAYLISTS,
+                    payload: response.data
+                });
+            })
+            .catch(error => {
+                dispatch({
+                    type: actions.error.UPDATE_ERROR,
+                    payload: error
+                });
+            });
+    };
+};
+
+export function getPlaylistById(id) {
+    return function(dispatch) {
+        Axios.get(`/api/playlists/${id}`)
+            .then(response => {
+                dispatch({
+                    type: actions.playlists.get.GET_BY_ID,
+                    payload: response.data
+                });
+            })
+            .catch(error => {
+                dispatch({
+                    type: actions.error.UPDATE_ERROR,
+                    payload: error
+                });
+            });
+    };
+};
