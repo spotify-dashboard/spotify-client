6d88dd95119d325f21deabb8b74fb549ea0d31b6
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-24 17:20:30 -0600

    reducer for getting all playlists

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import profileReducer from './profileReducer.js';
 import musicLibraryTracksReducer from './musicLibraryTracksReducer.js';
 import pageChangeReducer from './pageChangeReducer.js';
 import currentSongReducer from './currentSongReducer.js';
+import playlistReducer from './playlistReducer.js';
 
 export default combineReducers({
     getProfile: profileReducer,
     getMusicLibraryTracks: musicLibraryTracksReducer,
     pageChange: pageChangeReducer,
-    getCurrentSong: currentSongReducer
+    getCurrentSong: currentSongReducer,
+    getAllPlaylists: playlistReducer
 });
---
+++ client/src/reducers/playlistReducer.js
@@ -0,0 +1,23 @@
+import { actions } from '../actions/types.js';
+
+const initialState = {
+    allPlaylists: [],
+    playlist: []
+};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case actions.playlists.get.GET_ALL_PLAYLISTS:
+            return {
+                ...state,
+                allPlaylists: action.payload
+            }
+        case actions.playlists.get.GET_BY_ID:
+        return {
+            ...state,
+            playlist: action.payload
+        }
+        default:
+            return state;
+    }
+};
