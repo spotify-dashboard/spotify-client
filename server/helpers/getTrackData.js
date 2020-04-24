824514138c633979fa871fcc0aaa70d7c1663fa0
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-24 16:35:34 -0600

    updates to helper algo

--- server/helpers/getTrackData.js
+++ server/helpers/getTrackData.js
@@ -1,11 +1,11 @@
 const login = require ('../controllers/login.js'); // credentials
 const Axios = require('axios');
 
-module.exports.getTrackData = (url, params) => {
+module.exports.getTrackData = (url = 'https://api.spotify.com/v1/me/tracks', paylistId) => {
     return new Promise( async (resolve, reject) => {
 
         //set number of iterations for loop; will change after first api call to the max number of tracks
-        let totalTracks = 30;
+        let totalTracks = 1;
         //starting position for the call, will incrememt
         let offset = 1;
 
