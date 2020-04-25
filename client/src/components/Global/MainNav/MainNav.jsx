1cb97ac18d94e124998af001614f8b8f60f2e9d6
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-25 16:37:38 -0600

    added border on current page

--- client/src/components/Global/MainNav/MainNav.jsx
+++ client/src/components/Global/MainNav/MainNav.jsx
@@ -4,17 +4,18 @@ import { connect } from 'react-redux'; // connect to store
 import { pageChange } from '../../../actions/pageChangeActions.js';
 
 const MainNav = props => {
-    
+    console.log('main nav props', props)
     if (props.navItems !== undefined) {
         return (
             <nav className={styles.navbar}>
                 <ul className={styles.navList}>
                     {props.navItems.map(navItem => {
+
                         return (
                             <li 
                                 key={navItem.id}
                                 onClick={() => props.pageChange(navItem.navLink)} 
-                                className={styles.navItem}
+                                className={`${styles.navItem} ${navItem.navLink === props.currentPage ? styles.currentNavItem : ''}`}
                             >
                                     {navItem.navItem}
                             </li>
