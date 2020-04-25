a003c3d7009bba917100236f662b612a5ad1f064
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-25 11:43:18 -0600

    added reducer for login check

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,11 +4,13 @@ import musicLibraryTracksReducer from './musicLibraryTracksReducer.js';
 import pageChangeReducer from './pageChangeReducer.js';
 import currentSongReducer from './currentSongReducer.js';
 import playlistReducer from './playlistReducer.js';
+import loginReducer from './loginReducer.js';
 
 export default combineReducers({
+    loginCheck: loginReducer,
     getProfile: profileReducer,
     getMusicLibraryTracks: musicLibraryTracksReducer,
     pageChange: pageChangeReducer,
     getCurrentSong: currentSongReducer,
-    getAllPlaylists: playlistReducer
+    getAllPlaylists: playlistReducer,
 });
