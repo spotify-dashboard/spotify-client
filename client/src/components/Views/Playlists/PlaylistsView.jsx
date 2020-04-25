724a929eac16ef262019d35534f6431c80da1c39
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-25 11:43:09 -0600

    updates to playlist view

--- client/src/components/Views/Playlists/PlaylistsView.jsx
+++ client/src/components/Views/Playlists/PlaylistsView.jsx
@@ -7,18 +7,39 @@ import { musicLibraryNavItems } from '../../../NavItems.js';
 
 import MainNav from '../../Global/MainNav/MainNav.jsx';
 import Banner from '../../Global/Banner/Banner.jsx';
+import LoginError from '../../Global/LoginError/LoginError.jsx';
 
 const PlaylistsView = props => {
-
-    console.log('most recent view props', props)
+    console.log('playlist view props', props)
 
     return (
         <div className="mainSection">
             <div className="mainModule">
                 <Banner />
                 <MainNav navItems={musicLibraryNavItems} />
-                <h1>Playlists</h1>
-                <h3>In Development. Playlists coming soon!</h3>
+                {props.allPlaylists[0] === undefined &&
+                    <LoginError />
+                }
+                {props.allPlaylists[0] !== undefined &&
+                <div className={styles.playlistsContainer}>
+                    {props.allPlaylists.map(playlist => {
+                        return (
+                            <div className={styles.playlistItem}>
+                                {playlist.images[0] !== undefined &&
+                                <img className={styles.playlistImg} src={playlist.images[0].url} alt={playlist.description} />
+                                }
+                                <div className={styles.playlistItemRight}>
+                                    <div>
+                                    <h2>{playlist.name}</h2>
+                                    <h4 className="textGrey">{playlist.owner.display_name}</h4>
+                                    <p className="textGrey">{playlist.tracks.total} songs</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+                }
             </div>
         </div>
     )
@@ -27,6 +48,7 @@ const PlaylistsView = props => {
 const mapStateToProps = (state, ownProps) => {
     return {
         currentPage: state.pageChange.currentPage,
+        allPlaylists: state.getAllPlaylists.allPlaylists
     };
 };
 
