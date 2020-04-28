d008e89d9d3c018159f334ebc3a2f607bfc3c73f
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-28 10:45:47 -0600

    created song cache

--- server/controllers/playlists.js
+++ server/controllers/playlists.js
@@ -1,6 +1,9 @@
 const { getAllPlaylists } = require('../helpers/getAllPlaylists.js');
 const { getTrackData } = require('../helpers/getTrackData.js');
 
+// song cache to save playlists, cut down on api calls
+const songCache = {};
+
 module.exports = {
     // get all of current user's playlists
     all: {
@@ -18,15 +21,28 @@ module.exports = {
     // get a playlist by id
     playlist: {
         get: (req, res) => {
-            console.log(req.params.id);
-            getTrackData(`https://api.spotify.com/v1/playlists/${req.params.id}/tracks`)
+            //if songs are not cached
+            if (!songCache.hasOwnProperty(req.params.id)) {
+                console.log('cache does not include playlist');
+                //get songs and save in cache
+                getTrackData(`https://api.spotify.com/v1/playlists/${req.params.id}/tracks`)
                 .then(response => {
+                    songCache[req.params.id] = response;
                     res.status(200).json(response);
                 })
                 .catch(err => {
                     console.log("Error in playlists get by id", err);
                     res.status(400).json({message: "Error getting playlist data", error: err});
                 });
+            } else if (songCache.hasOwnProperty(req.params.id)) {
+                // if songs are in cache
+                console.log('Song cache includes playlist');
+                // serve cached songs
+                res.status(304).json(songCache[req.params.id])
+            } else {
+                console.log('song cache error');
+                res.status(400).json({message: 'Error retrieving songs from cache'});
+            }
         }
     }
 }
