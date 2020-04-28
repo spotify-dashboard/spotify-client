786b3b3b8fa9c4dd3fc8b11173cae26515f5308b
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-28 16:28:55 -0600

    created logout button

---
+++ client/src/components/Global/Logout/Logout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './logout.module.scss';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { logout } from '../../../actions/logoutActions.js';
+import { pageChange } from '../../../actions/pageChangeActions.js';
+
+const Login = props => {
+    return (
+        <div className="logoutModule">
+            <Link to="/" >
+                <button onClick={() => { props.logout(); props.pageChange('/logout') }} className="green-btn">Logout</button>
+            </Link>
+        </div>
+    )
+};
+
+const mapStateToProps = (state, ownProps) => {
+    return {};
+};
+
+const mapDispatchToProps = {
+    logout,
+    pageChange
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
