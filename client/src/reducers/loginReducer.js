3d2d74260518a15bcc7b44ec767bdfe56e6d442b
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-28 13:04:22 -0600

    added logout logic in login reducer

--- client/src/reducers/loginReducer.js
+++ client/src/reducers/loginReducer.js
@@ -7,17 +7,27 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        // login
         case actions.login.LOGIN_CHECK:
             return {
                 ...state,
                 loggedIn: action.payload,
                 error: null
             }
+        // logout
+        case actions.logout:
+        return {
+            ...state,
+            loggedIn: action.payload,
+            error: null
+        }
+        // err
         case actions.error.UPDATE_ERROR:
             return {
                 ...state,
                 error: action.payload
             }
+        
         default:
             return state;
     }
