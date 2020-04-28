f2e872d2a0fe05fbc02e550d844a88aed3df6c3e
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-28 16:29:20 -0600

    removing state from local storage and refreshing page

--- client/src/actions/logoutActions.js
+++ client/src/actions/logoutActions.js
@@ -8,10 +8,17 @@ export function logout() {
     return function(dispatch) {
         Axios.get('/logout/')
                 .then(response => {
+                    
+                    // removes access token
                     dispatch({
                         type: actions.logout.LOGOUT,
                         payload: response.data
                     });
+
+                    //reload page to see changes
+                    window.location.reload();
+                    //remove state from local storage
+                    localStorage.removeItem('state');
                 })
                 .catch(error => {
                     dispatch({
