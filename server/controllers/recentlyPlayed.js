9a6089ad0a0ad3df1fe4999551f86dbc0c863930
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-29 18:21:43 -0600

    cleaning data to serve

--- server/controllers/recentlyPlayed.js
+++ server/controllers/recentlyPlayed.js
@@ -1,13 +1,19 @@
 const  { getTrackData } = require('../helpers/getTrackData.js');
+const { getArtistData } = require('../helpers/getArtistData.js');
 
 module.exports = {
     recently_played: {
         get: (req, res) => {
 
+            // get the tracks, pass in 50 as optional limit since you can ony get 50 recently played songs
             getTrackData('https://api.spotify.com/v1/me/player/recently-played', 50)
                 .then(results => {
-                    console.log('recent tracks results', results)
-                    res.status(200).json(results);
+                    // get artist data for each track and return
+                    return getArtistData(results)    
+                })
+                .then(data => {
+                    // console.log(data);
+                    res.status(200).json(data);
                 })
                 .catch(err => {
                     console.log("Error from getTrackData Promise", err);
@@ -16,3 +22,5 @@ module.exports = {
         }
     }
 }
+
+// USE new Date(UTC DATE that you get back from recently playeds)
