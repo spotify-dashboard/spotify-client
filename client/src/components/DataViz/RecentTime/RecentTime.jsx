3d5078250db3e8d2805b7d45385ef31c2292f0fc
Brandon Ellis <brandonellis.email@gmail.com>
2020-05-01 21:55:07 -0600

    building radar chart with recent time listened

--- client/src/components/DataViz/RecentTime/RecentTime.jsx
+++ client/src/components/DataViz/RecentTime/RecentTime.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
 import styles from './recenttime.module.scss';
 import { connect } from 'react-redux';
+import { Chart } from 'chart.js';
+import ReactDOM from 'react-dom';
 
-const RecentTime = props => {
+class RecentTime extends React.Component {
 
-    // console.log(props.recentTracks)
+    componentDidMount() {
+        this.initializeTree();
+        this.convertDates(this.props.recentTracks);
+    }
 
-    
-    const convertDates = (arr) => {
+    initializeTree() {
+        let el = ReactDOM.findDOMNode(this.refs.recentTimeRadar);
+        let ctx = el.getContext("2d");
+        let radar = new Chart(ctx, {
+            type: "radar",
+            data: {
+                labels: Object.keys(this.convertDates(this.props.recentTracks)), // points on radar
+                datasets: [
+                    {
+                        data: Object.values(this.convertDates(this.props.recentTracks)),
+                        fontColor: 'rgb(213,116,159)',
+                        fontSize: 14,
+                        fontStyle: 'normal',
+                        backgroundColor: function(ctx) {
+                            var value = ctx.dataset.data[ctx.dataIndex];
+                            var alpha = (value + 3) / 10;
+                            return Color('rgb(41,53,99)').alpha(alpha).rgbString();
+                        },
+                    }
+                ]
+            },
+            options: {
+                maintainAspectRatio: false,
+                title: {
+                    display: true,
+                    text: "Listening by time of day"
+                },
+                legend: {
+                    display: false
+                }
+            }
+        });
+    }
 
-        // categories of listening times; destructured 
-        let [early, preNoon, noon, afternoon, earlyEvening, evening, night, lateNight] = [['early',0],['pre-noon',0],['noon',0],['afternoon',0],['early evening',0],['evening',0],['night',0],['late night',0]];
+    convertDates(arr) {
+
         let timeOfDayObj = {
             early: 0,
             preNoon: 0,
@@ -21,7 +57,6 @@ const RecentTime = props => {
             night: 0,
             lateNight: 0
         }
-        
         // iterate through tracks
         for (let i = 0; i < arr.length; i++) {
             // convert dates and push to arr
@@ -56,30 +91,41 @@ const RecentTime = props => {
             }
         }
 
-        // find largest value in obj
-        const getMax = object => {
-    
-            // return an array of top repeat song(s)
-            return Object.keys(object).filter(x => {
-                 return object[x] == Math.max.apply(null, 
-                 Object.values(object));
-           });
-        };
-
-        //return array of times
-        return getMax(timeOfDayObj);
+        return timeOfDayObj;
     }
 
-    return (
-        <div className={styles.parentContainer}>
-            <h3>Time of Day | When have you been listening most often?</h3>
-            {convertDates(props.recentTracks).map(time => {
-                return (
-                <h1 key={convertDates(props.recentTracks).indexOf(time)} className={styles.dateItem}>{time}</h1>
-                )
-            })}
-        </div>
-    )
+    // find largest value in obj
+    getMax(object) {
+        // return an array of top repeat song(s)
+        return Object.keys(object).filter(x => {
+            return object[x] == Math.max.apply(null, 
+            Object.values(object));
+        });
+    }
+
+    render() {
+
+        console.log(this.convertDates(this.props.recentTracks))
+
+        return (
+            <div className={styles.parentContainer}>
+                <h3>Time of Day | When have you been listening most often?</h3>
+                <div>
+                {this.getMax(this.convertDates(this.props.recentTracks)).map(time => {
+                    return (
+                    <h1 key={this.getMax(this.convertDates(this.props.recentTracks)).indexOf(time)} className={styles.dateItem}>{time}</h1>
+                    )
+                })}
+                <canvas
+                    ref="recentTimeRadar"
+                    className={styles.graphContainer}
+                >
+                </canvas>
+                </div>
+            </div>
+        )
+
+    }
 };
 
 const mapStateToProps = (state, ownProps) => {
