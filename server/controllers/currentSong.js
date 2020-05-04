56574e9b2059d2695b14a93af931c830c8c818ed
Brandon Ellis <brandonellis.email@gmail.com>
2020-05-04 14:29:06 -0600

    tried to implement volume change on ad, only available for premium

--- server/controllers/currentSong.js
+++ server/controllers/currentSong.js
@@ -1,11 +1,16 @@
 const login = require('../controllers/login.js');
+const Axios = require('axios');
 
 module.exports = {
     currentSong: {
-        get: (req, res) => {
-            login.credentials.getMyCurrentPlaybackState({})
-                .then(data => {
-                    res.status(200).json(data.body)
+        get: async (req, res) => {
+            await Axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
+                headers: {
+                    "Authorization": 'Bearer ' + login.credentials._credentials.accessToken
+                }
+            })
+                .then( data => {
+                    res.status(200).json(data.data);
                 })
                 .catch(err => {
                     console.log('Error getting current song', err);
