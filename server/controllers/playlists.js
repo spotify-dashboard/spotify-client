2a3ff899e1203786b5f6815d8df6ffed4dcd2718
Brandon Ellis <brandonellis.email@gmail.com>
2020-05-06 01:01:18 -0600

    created cache for all playlists

--- server/controllers/playlists.js
+++ server/controllers/playlists.js
@@ -4,18 +4,30 @@ const { getTrackData } = require('../helpers/getTrackData.js');
 // song cache to save playlists, cut down on api calls
 const songCache = {};
 
+//save list of all playlists; cached based on the hour
+const playlistsCache = {};
 module.exports = {
     // get all of current user's playlists
     all: {
         get: (req, res) => {
+            if (!playlistsCache.hasOwnProperty(new Date().getHours())) {
             getAllPlaylists('https://api.spotify.com/v1/me/playlists')
                 .then(results => {
+                    // set cache property to current hour
+                    playlistsCache[new Date().getHours()] = results;
                     res.status(200).json(results);
                 })
                 .catch(err => {
                     console.log('Error in Playlist controller', err);
                     res.status(400).json({message: "Error getting all playlists", error: err})
                 });
+            } else if (playlistsCache.hasOwnProperty(new Date().getHours())) {
+                console.log('serving cached playlists list')
+                res.status(304).json(playlistsCache[new Date().getHours()])
+            } else {
+                console.log('playlists cache error');
+                res.status(400).json({message: 'Error retrieving playlists from cache'});
+            }
         }
     },
     // get a playlist by id
