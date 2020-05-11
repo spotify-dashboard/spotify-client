6bbebea4245b39d6c0baf2fbedcd81bdf597c6d3
Brandon Ellis <brandonellis.email@gmail.com>
2020-05-11 14:28:24 -0600

    starting to process playlist data

--- server/controllers/playlist_breakdown.js
+++ server/controllers/playlist_breakdown.js
@@ -1,6 +1,3 @@
-const login = require('../controllers/login.js');
-const Axios = require('axios');
-
 const { getAllPlaylists } = require('../helpers/getAllPlaylists.js');
 const { getTrackData } = require('../helpers/getTrackData.js');
 const { getArtistData } = require('../helpers/getArtistData.js');
@@ -12,8 +9,6 @@ var playlistCache = {
     // key names are playlist ids
         // each key's value is an object containing
             // tracks array
-
-
 };
 
 module.exports = {
@@ -21,9 +16,110 @@ module.exports = {
         // specific playlist breakdown
         playlist: async (req, res) => {
 
+            // holds returned data
+            let completeTrackData = {
+                tracks: [],
+                genres: []
+            };
+
+            let tracksArr;
+            let artistsArr;
+            let genresArr;
+            let featuresArr;
+
+            // console.log(req.params.id)
             // get tracks; pass in url with playlist id
+            await getTrackData(`https://api.spotify.com/v1/playlists/${req.params.id}/tracks`)
+                .then(tracks => {
+                    // set for part of return data
+                    tracksArr = tracks;
+                    console.log('+++', tracks)
+                    // pass track data into func to get artists for tracks
+                    return getArtistData(tracks);
+                })
+                .then(artistsData => {
+                    // set for part of return data
+                    artistsArr = artistsData;
+                    // pass artists into func to get genres
+                    return getGenreData(artistsData);
+                })
+                .then(genres => {
+                    // set for part of return data
+                    genresArr = genres;
+                })
+                .then(() => {
+                    // get audio features for all songs
+                    return getAudioFeatures(tracksArr);
+                })
+                .then(features => {
+                    // set features for return data
+                    featuresArr = features;
+                    res.json(artistsArr)
+                })
+                .catch(err => {
+                    console.log("Error in individual playlist breakdown");
+                    res.status(400).json({message: "Error", error: err});
+                });
 
+            
+            // // function to flatten genres array and tally genres
+            // const createGenreObject = async () => {
+                
+            //     // creating genre obj to contain multiple genre views
+            //     let genreObject = {}
 
+            //     // tally of top genres to be added to genre obj
+            //     let genreTally = {};
+
+            //     // splits each genre into an object of artist name and num of listens
+            //     let genreArrayOfObjects = [];
+                
+            //     // flatten genres array
+            //     let flattenedGenres = genresArr.flat(Infinity);
+
+            //     //iterate through all genres and add to tally
+            //     await flattenedGenres.forEach(genre => {
+            //         if (!genreTally.hasOwnProperty(genre)) {
+            //             genreTally[genre] = 1;
+            //         } else if (genreTally.hasOwnProperty(genre)) {
+            //             genreTally[genre]++;
+            //         } else {
+            //             console.log("Error tallying genres");
+            //         }
+            //     });
+                
+            //     // add tally view to genre object
+            //     genreObject.tally = genreTally;
+
+            //     for (let [key, value] of Object.entries(genreTally)) {
+            //         genreArrayOfObjects.push({ genre: key, listens: value });
+            //     }
+
+            //     // add genre objects to parent obj
+            //     genreObject.genre_objects = genreArrayOfObjects;
+
+            //     // add genre object to the complete genre that will be served
+            //     completeTrackData.genres = genreObject;
+            // };
+
+            // await createGenreObject();
+
+            // // add features to return obj
+            // completeTrackData.features = featuresArr;
+
+            // // push track, played_at, artist, and genres for each track
+            // for (let i = 0; i < genresArr.length; i++) {
+            //     completeTrackData.tracks.push({
+            //         track: tracksArr[i],
+            //         played_at: artistsArr.playedAtDates[i],
+            //         artist: artistsArr.artists[i],
+            //         genres: genresArr[i],
+            //     });
+            // };
+
+            // console.log(completeTrackData)
+            // //return
+            // res.status(200).json(completeTrackData);
         },
         // for aggregating all playlists
         allPlaylists: async (req, res) => {
@@ -43,7 +139,7 @@ module.exports = {
                 .catch(err => {
                     console.log('Error in allPlaylists breakdown', err);
                     res.status(400).json({message: "Error", error: err});
-                })
+                });
         }
     }
 };
