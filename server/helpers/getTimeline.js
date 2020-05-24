2be456234ad2d9bf00ae9089e6abf88df5c6cfb9
Brandon Ellis <brandonellis.email@gmail.com>
2020-05-24 16:27:15 -0600

    moved data formatting and color selected to server

--- server/helpers/getTimeline.js
+++ server/helpers/getTimeline.js
@@ -1,3 +1,38 @@
+
+let index = 0;
+// randomized background colors for the chart
+let selectColor = () => {
+    // Spotify branding colors
+    let colors = [
+        "rgb(34,176,67)",
+        "rgb(41,53,99)", 
+        "rgb(213,116,159)",
+        "rgb(108,154,243)",
+        "rgb(207,239,185)",
+        "rgb(121,30,53)",
+        "rgb(183,241,226)",
+        "rgb(224,99,49)",
+        "rgb(242,232,54)",
+        "rgb(56,101,82)",
+        "rgb(56,0,244)",
+        "rgb(241,203,209)"
+    ];
+    if (index === 0) {
+        index++;
+        return colors[0]
+    } else if (index === colors.length) {
+        // reset to 0
+        index = 0;
+        // return first item
+        return colors[index];
+
+    } else if (index > 0 && index < colors.length) {
+        index++;
+        return colors[index - 1];
+    }
+}
+
+
 module.exports.getTimeline = (musicArray) => {
     return new Promise( async (resolve, reject) => {
         
@@ -13,6 +48,7 @@ module.exports.getTimeline = (musicArray) => {
                     //{ "2-6-2018": 0 }
                 // ]
             },
+            formattedData: []
         };
 
         const dateConverter = (rawDate) => {
@@ -66,6 +102,16 @@ module.exports.getTimeline = (musicArray) => {
                 // increment playlist obj 
                 timelineObj.playlists[currentPlaylist][currentDate]++;
             }
+        }
+
+        // iterate through data object
+        for (let [key, value] of Object.entries(timelineObj.playlists)) {
+            let dataArray = Object.values(timelineObj.playlists[key]);
+            timelineObj.formattedData.push({
+                label: key,
+                backgroundColor: selectColor(),
+                data: dataArray
+            });
         }
 
         resolve(timelineObj);
