feab0faa6f02559ca8db55fc4fe0a4503a6da359
Brandon Ellis <brandonellis.email@gmail.com>
2020-05-29 18:04:15 -0600

    added description

--- client/src/components/DataViz/Playlists/All/Timeline/Timeline.jsx
+++ client/src/components/DataViz/Playlists/All/Timeline/Timeline.jsx
@@ -60,7 +60,7 @@ class Timeline extends React.Component {
                 <div className="flex flex-vertical-align flex-spread">
                     <div>
                         <h3>Timeline</h3>
-                        <p>Dates when tracks were added, grouped by playlist</p>
+                        <p>Dates when tracks were added, grouped by each of your personal playlists</p>
                     </div>
                     <div>
                         <p>Sort Options</p>
