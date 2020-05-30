0736f4f039dabc2778052067caa4694553e01082
Brandon Ellis <brandonellis.email@gmail.com>
2020-05-29 18:17:47 -0600

    displaying genre bar chart and tree map

--- client/src/components/Views/Playlist_Breakdown/BreakdownView/BreakdownView.jsx
+++ client/src/components/Views/Playlist_Breakdown/BreakdownView/BreakdownView.jsx
@@ -15,6 +15,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Timeline from '../../../DataViz/Playlists/All/Timeline/Timeline.jsx';
 import GenreTreeMap from '../../../DataViz/Playlists/All/GenreTreeMap/GenreTreeMap.jsx';
+import GenreBarChart from '../../../DataViz/Playlists/All/GenreBarChart/GenreBarChart.jsx';
 
 import { breakdownAllPlaylists } from '../../../../actions/breakdownActions.js';
 
@@ -100,9 +101,11 @@ const PlaylistsView = props => {
                                 {/* DATA LOADED VIEW */}
                                 {!Array.isArray(props.breakdownAll) &&
                                     <div>
+                                        <GenreTreeMap />
+                                        <GenreBarChart />
+                                        <div className="divider"></div>
                                         <Timeline />
                                         <div className="divider"></div>
-                                        <GenreTreeMap />
                                     </div>
                                 }
                             </div>
