727ffff4880afbd2768a41aba6b2a0b088eeda3a
Brandon Ellis <brandonellis.email@gmail.com>
2020-06-05 19:03:35 -0600

    rendering features radar

--- client/src/components/Views/Playlist_Breakdown/BreakdownView/BreakdownView.jsx
+++ client/src/components/Views/Playlist_Breakdown/BreakdownView/BreakdownView.jsx
@@ -16,6 +16,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Timeline from '../../../DataViz/Playlists/All/Timeline/Timeline.jsx';
 import GenreTreeMap from '../../../DataViz/Playlists/All/GenreTreeMap/GenreTreeMap.jsx';
 import GenreBarChart from '../../../DataViz/Playlists/All/GenreBarChart/GenreBarChart.jsx';
+import FeaturesRadar from '../../../DataViz/Playlists/All/FeaturesRadar/FeaturesRadar.jsx';
 
 import { breakdownAllPlaylists } from '../../../../actions/breakdownActions.js';
 
@@ -105,11 +106,13 @@ const PlaylistsView = props => {
                                             <h1>All Playlists</h1>
                                             <p>A breakdown of the trends found after analyzing all of your personal playlists.</p>
                                         </div>
+                                        <div className="divider"></div>
                                         <GenreTreeMap />
                                         <GenreBarChart />
                                         <div className="divider"></div>
                                         <Timeline />
-                                        <div className="divider"></div>
+                                        {/* <div className="divider"></div> */}
+                                        <FeaturesRadar />
                                     </div>
                                 }
                             </div>
