7e115d303da1662e39421bf0af2a3c56a0d6c9e3
Brandon Ellis <brandonellis.email@gmail.com>
2020-06-05 19:04:01 -0600

    finished features radar

---
+++ client/src/components/DataViz/Playlists/All/FeaturesRadar/FeaturesRadar.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import styles from './featuresradar.module.scss';
+import { connect } from 'react-redux';
+import { Chart } from 'chart.js';
+import ReactDOM from 'react-dom';
+
+class FeaturesRadar extends React.Component {
+
+    componentDidMount() {
+        this.initializeRadar();
+    }
+
+    initializeRadar() {
+        let el = ReactDOM.findDOMNode(this.refs.featuresRadar);
+        let ctx = el.getContext("2d");
+        let radar = new Chart(ctx, {
+            type: "radar",
+            data: {
+                labels: Object.keys(this.props.audioFeaturesTally), // points on radar
+                datasets: [
+                    {
+                        data: Object.values(this.props.audioFeaturesTally),
+                        fontColor: 'rgb(213,116,159)',
+                        fontSize: 14,
+                        pointBorderColor: "#fff",
+                        pointBackgroundColor: "rgba(255,99,132,1)",
+                        borderColor: "rgba(179,181,198,1)",
+                        fontStyle: 'normal',
+                        backgroundColor: function(ctx) {
+                            var value = ctx.dataset.data[ctx.dataIndex];
+                            var alpha = (value + 3) / 10;
+                            return Color('rgb(41,53,99)').alpha(alpha).rgbString();
+                        },
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                title: {
+                    display: true,
+                    text: "Audio Characteristics"
+                },
+                legend: {
+                    display: false
+                },
+                scale: {
+                    gridLines: {
+                        color: ['rgb(213,116,159)','rgb(213,116,159)','rgb(213,116,159)','rgb(213,116,159)','rgb(213,116,159)','rgb(213,116,159)']
+                    },
+                    ticks: {
+                       display: false,
+                       max: 1.0,
+                       min: 0,
+                       stepSize: 0.2
+                    },
+                    pointLabels: {
+                        fontSize: 20
+                    },
+                },
+                tooltips: {
+                    callbacks: {
+                        title: (item, data) => {
+                            return '';
+                        },
+                        label: (item, data) => {
+                            return '';
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    // find largest value in obj
+    getMax(object) {
+        // return an array of top repeat song(s)
+        return Object.keys(object).filter(x => {
+            return object[x] == Math.max.apply(null, 
+            Object.values(object));
+        });
+    }
+
+    render() {
+        console.log('features radar props', this.props)
+        return (
+            <div className={styles.parentContainer}>
+                <h3>Audio Characterists | What audio features are usually present in your songs?</h3>
+                <div className="flex flex-justify-center">
+                    <p><span>acousticness</span> - measure of how acoustic the tracks are</p>
+                    <p><span>danceability</span> - describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity</p>
+                    <p><span>energy</span> - represents a perceptual measure of intensity and activity (energetic tracks feel fast, loud, and noisy)</p>
+                </div>
+                <div className="flex flex-vertical-align">    
+                    <div className={styles.left}>
+                        <p><span>instrumentalness</span> - predicts whether a track contains no vocals</p>
+                        <br />
+                        <p><span>liveness</span> - detects the presence of an audience in the recording (live performance)</p>
+                        <br />
+                        <p><span>speechiness</span> - detects the presence of spoken words in a track (talk show, audio book, etc.)</p>
+                    </div>
+                    <canvas
+                        ref="featuresRadar"
+                        className={styles.graphContainer}
+                    >
+                    </canvas>
+                </div>
+            </div>
+        )
+
+    }
+};
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        audioFeaturesTally: state.getPlaylistBreakdown.breakdownAll.features.featuresTally,
+    }
+};
+
+export default connect(mapStateToProps)(FeaturesRadar);
