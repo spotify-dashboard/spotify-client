855f7a2470473eab41e3ea868715f995b3c2c100
Brandon Ellis <brandonellis.email@gmail.com>
2020-06-07 14:25:55 -0600

    current playlist reducer updated

--- client/src/reducers/playlistReducer.js
+++ client/src/reducers/playlistReducer.js
@@ -2,7 +2,7 @@ import { actions } from '../actions/types.js';
 
 const initialState = {
     allPlaylists: [],
-    playlist: [],
+    currentPlaylist: undefined,
     error: null
 };
 
@@ -15,22 +15,22 @@ export default function(state = initialState, action) {
                 error: null
             }
         case actions.playlists.get.GET_BY_ID:
-        return {
-            ...state,
-            playlist: action.payload,
-            error: null
-        }
+            return {
+                ...state,
+                currentPlaylist: action.payload,
+                error: null
+            }
         case actions.playlists.clear.CLEAR_PLAYLIST:
-        return {
-            ...state,
-            playlist: action.payload,
-            error: null
-        }
+            return {
+                ...state,
+                playlist: action.payload,
+                error: null
+            }
         case actions.error.UPDATE_ERROR:
-        return {
-            ...state,
-            error: action.payload
-        }
+            return {
+                ...state,
+                error: action.payload
+            }
         default:
             return state;
     }
