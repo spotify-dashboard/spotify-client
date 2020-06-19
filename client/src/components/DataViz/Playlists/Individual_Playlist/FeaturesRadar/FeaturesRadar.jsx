b5f4c8457baafcbf6d38bc25f64fe3f5cfaf2c1f
Brandon Ellis <brandonellis.email@gmail.com>
2020-06-19 13:45:33 -0600

    fixed class styling conficts with span

--- client/src/components/DataViz/Playlists/Individual_Playlist/FeaturesRadar/FeaturesRadar.jsx
+++ client/src/components/DataViz/Playlists/Individual_Playlist/FeaturesRadar/FeaturesRadar.jsx
@@ -83,17 +83,17 @@ class FeaturesRadarIndividual extends React.Component {
             <div className={styles.parentContainer}>
                 <h3>Audio Characterists | What audio features are usually present in your songs?</h3>
                 <div className="flex flex-justify-center">
-                    <p><span className="featureTitleIndiv">acousticness</span> - measure of how acoustic the tracks are</p>
-                    <p><span className="featureTitleIndiv">danceability</span> - describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity</p>
-                    <p><span className="featureTitleIndiv">energy</span> - represents a perceptual measure of intensity and activity (energetic tracks feel fast, loud, and noisy)</p>
+                    <p><span className={styles.featureTitleIndiv}>acousticness</span> - measure of how acoustic the tracks are</p>
+                    <p><span className={styles.featureTitleIndiv}>danceability</span> - describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity</p>
+                    <p><span className={styles.featureTitleIndiv}>energy</span> - represents a perceptual measure of intensity and activity (energetic tracks feel fast, loud, and noisy)</p>
                 </div>
                 <div className="flex flex-vertical-align">    
                     <div className={styles.left}>
-                        <p><span className="featureTitleIndiv">instrumentalness</span> - predicts whether a track contains no vocals</p>
+                        <p><span className={styles.featureTitleIndiv}>instrumentalness</span> - predicts whether a track contains no vocals</p>
                         <br />
-                        <p><span className="featureTitleIndiv">liveness</span> - detects the presence of an audience in the recording (live performance)</p>
+                        <p><span className={styles.featureTitleIndiv}>liveness</span> - detects the presence of an audience in the recording (live performance)</p>
                         <br />
-                        <p><span className="featureTitleIndiv">speechiness</span> - detects the presence of spoken words in a track (talk show, audio book, etc.)</p>
+                        <p><span className={styles.featureTitleIndiv}>speechiness</span> - detects the presence of spoken words in a track (talk show, audio book, etc.)</p>
                     </div>
                     <canvas
                         ref="featuresRadarIndividual"
--- client/src/components/DataViz/Recently_Played/FeaturesRadar/FeaturesRadar.jsx
+++ client/src/components/DataViz/Recently_Played/FeaturesRadar/FeaturesRadar.jsx
@@ -87,17 +87,17 @@ class FeaturesRadar extends React.Component {
             <div className={styles.parentContainer}>
                 <h3>Audio Characterists | What audio features are usually present in your songs?</h3>
                 <div className="flex flex-justify-center">
-                    <p><span className="featureTitleRecent">acousticness</span> - measure of how acoustic the tracks are</p>
-                    <p><span className="featureTitleRecent">danceability</span> - describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity</p>
-                    <p><span className="featureTitleRecent">energy</span> - represents a perceptual measure of intensity and activity (energetic tracks feel fast, loud, and noisy)</p>
+                    <p><span className={styles.featureTitleRecent}>acousticness</span> - measure of how acoustic the tracks are</p>
+                    <p><span className={styles.featureTitleRecent}>danceability</span> - describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity</p>
+                    <p><span className={styles.featureTitleRecent}>energy</span> - represents a perceptual measure of intensity and activity (energetic tracks feel fast, loud, and noisy)</p>
                 </div>
                 <div className="flex flex-vertical-align">    
                     <div className={styles.left}>
-                        <p><span className="featureTitleRecent">instrumentalness</span> - predicts whether a track contains no vocals</p>
+                        <p><span className={styles.featureTitleRecent}>instrumentalness</span> - predicts whether a track contains no vocals</p>
                         <br />
-                        <p><span className="featureTitleRecent">liveness</span> - detects the presence of an audience in the recording (live performance)</p>
+                        <p><span className={styles.featureTitleRecent}>liveness</span> - detects the presence of an audience in the recording (live performance)</p>
                         <br />
-                        <p><span className="featureTitleRecent">speechiness</span> - detects the presence of spoken words in a track (talk show, audio book, etc.)</p>
+                        <p><span className={styles.featureTitleRecent}>speechiness</span> - detects the presence of spoken words in a track (talk show, audio book, etc.)</p>
                     </div>
                     <canvas
                         ref="featuresRadar"
